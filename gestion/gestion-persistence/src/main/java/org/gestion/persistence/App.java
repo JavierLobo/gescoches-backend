3a80d8b2a36dae46527601bf0b7a9789715d9cb2
javierlobo <franciscojavier.lobo@gmail.com>
2020-01-22 23:37:15 +0100

    (DEV) Add gestion service

---
+++ gestion/gestion-persistence/src/main/java/org/gestion/persistence/App.java
@@ -0,0 +1,13 @@
+package org.gestion.persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
