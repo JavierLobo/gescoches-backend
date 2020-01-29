832bfa29c0d6ea4c740a45eac2b17474ae9204ff
javierlobo <franciscojavier.lobo@gmail.com>
2020-01-29 22:29:15 +0100

    (DEV) Change the structure of the packages
    (DEV) Cambios en la estructura de los paquete para que esté organizada
    (CFG) Corrección en la configuración del archivo 'application.yml'

--- config-server/src/main/java/com/javierlobo/gescoches/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/javierlobo/gescoches/configserver/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.javierlobo.gesCoches.config_server;
+package com.javierlobo.gescoches.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
