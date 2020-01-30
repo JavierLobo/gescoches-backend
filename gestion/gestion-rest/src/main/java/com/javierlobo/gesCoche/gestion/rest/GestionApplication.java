c70ca317344d98499a5fe0bc57e84f0d4f78bbc5
javierlobo <franciscojavier.lobo@gmail.com>
2020-01-30 13:27:07 +0100

    (CFG) Correct configuration to enable the service

--- gestion/gestion-rest/src/main/java/com/javierlobo/gesCoche/gestion/rest/GestionApplication.java
+++ gestion/gestion-rest/src/main/java/com/javierlobo/gesCoche/gestion/rest/GestionApplication.java
@@ -1,11 +1,17 @@
-package com.javierlobo.gesCoche.gestion.rest;
+package com.javierlobo.gescoche.gestion.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan({"com.javierlobo.gescoche"})
+@EntityScan("com.javierlobo.gescoche")
+@EnableMongoRepositories("com.javierlobo.gescoche")
 public class GestionApplication 
 {
     public static void main( String[] args )
